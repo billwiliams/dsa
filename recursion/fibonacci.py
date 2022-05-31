53f63d4537c51629952cf91ade8a57cc5a2e3031
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-05-31 09:36:20 +0300

    change base case to introduce zero

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -2,8 +2,12 @@
 
 def fibonacci(n):
     #base case
-    if n==2 or n==1:
-        return 1
+    if n==0 or n==1:
+        return n
     return fibonacci(n-1)+fibonacci(n-2)
 
 print(fibonacci(10))
+print(fibonacci(3))
+print(fibonacci(2))
+print(fibonacci(0))
+print(fibonacci(1))
