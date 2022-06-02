157457312925eae6caafc97318e72c6573300395
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-02 10:22:28 +0300

    print recusively in both ascending and descending format

--- recursion/print_numbers.py
+++ recursion/print_numbers.py
@@ -1,17 +1,24 @@
 #Recursively Print numbers from 1 to n
 
-def print_numbers(n):
+def print_numbers_ascending(n):
     #base case 
-    print(n)
-    if n==1:
-        return
     
-    return print_numbers(n-1)
-
-print_numbers(n=10)
-
-def print_numbers_recursively(n):
     if n==1:
-        return n
-    return n , print_numbers_recursively(n-1)
-print(print_numbers_recursively(10))
+        print(n)
+        return 
+    
+    print_numbers_ascending(n-1)
+    print(n)
+
+
+
+print_numbers_ascending(n=10)
+
+def print_numbers_descending(n):
+    if n==1:
+        print(n)
+        return
+    print(n)
+    print_numbers_descending(n-1)
+
+print_numbers_descending(10)
