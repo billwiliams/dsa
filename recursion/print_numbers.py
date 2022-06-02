85f8051705b01fcf6b5472b2ba2be1ae6377a5ab
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-02 07:43:49 +0300

    recursively print numbers from n to 1

--- recursion/print_numbers.py
+++ recursion/print_numbers.py
@@ -9,3 +9,9 @@ def print_numbers(n):
     return print_numbers(n-1)
 
 print_numbers(n=10)
+
+def print_numbers_recursively(n):
+    if n==1:
+        return n
+    return n , print_numbers_recursively(n-1)
+print(print_numbers_recursively(10))
