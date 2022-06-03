f7b0f6b624e82f507b481e330ebf0b2eb773ccae
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-03 19:42:20 +0300

    recursively compute sum of digits in a number

--- recursion/sum_of_digits.py
+++ recursion/sum_of_digits.py
@@ -1,4 +1,18 @@
 # Sum of digits in a number
 
 def sum_of_digits(n):
-    NotImplemented
+    
+    #Base Case
+    if n<1:
+        return 0
+    
+    small_sum=n%10
+
+    return small_sum +sum_of_digits(n//10)
+
+
+print(sum_of_digits(10))
+print(sum_of_digits(123))
+print(sum_of_digits(0))
+print(sum_of_digits(1010123))
+
