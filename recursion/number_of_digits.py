99b4feae742f8046d04d6c0dd20cc258a2635505
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-03 09:19:12 +0300

    recursively count the number of digits in an number

--- recursion/number_of_digits.py
+++ recursion/number_of_digits.py
@@ -1,6 +1,17 @@
-# Count number of digits in a number
+# Count number of digits in a  positive number
 
 def number_of_digits(n):
-    NotImplementedError
+    if n<0:
+        n= abs(n)
+    if n/10<1:
+        return 1
+    return 1 + number_of_digits(n//10)
+
+
+print(number_of_digits(0))
+print(number_of_digits(110))
+print(number_of_digits(305098843))
+print(number_of_digits(-305098843))
+
 
     
