da01e7b5d4b168fcc576ecbee859899ba0d61370
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-04 19:54:38 +0300

    check for precedding zeros

--- recursion/zeroes.py
+++ recursion/zeroes.py
@@ -10,7 +10,9 @@ def zeroes(n):
         divisibe_by_ten=True
     return int(divisibe_by_ten) +zeroes(n//10)
 
+
 print(zeroes(10))
 print(zeroes(10001))
 print(zeroes(12342343401))
 print(zeroes(100303400001))
+print(zeroes(10))
