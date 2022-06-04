1fbc1ca574d3401d4256cede2c63eb08caf81a56
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-04 19:52:12 +0300

    check how many zeroes are in a number

---
+++ recursion/zeroes.py
@@ -0,0 +1,16 @@
+# Count the numbers of zeroes recursively
+
+def zeroes(n):
+    
+    #Base Case
+    if n <=0:
+        return 0
+    divisibe_by_ten=False
+    if n%10==0:
+        divisibe_by_ten=True
+    return int(divisibe_by_ten) +zeroes(n//10)
+
+print(zeroes(10))
+print(zeroes(10001))
+print(zeroes(12342343401))
+print(zeroes(100303400001))
