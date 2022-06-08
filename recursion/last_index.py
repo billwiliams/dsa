55d9023c7130f34d187caaf7e6e7f409cecca9d2
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-08 19:54:31 +0300

    recursively get the last index of an item in an array

---
+++ recursion/last_index.py
@@ -0,0 +1,18 @@
+# recursively check for the last index of an element in an array
+
+
+def last_index(array,element,index):
+
+    if index<0:
+        return False
+    
+    else:
+        if array[index]==element:
+            return index
+        else:
+
+            return last_index(array,element,index-1)
+
+print(last_index([0,1,2,3,0],0,4))
+
+print(last_index([0,1,2,3],0,3))
