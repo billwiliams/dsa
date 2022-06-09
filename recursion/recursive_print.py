23d956fe58c979612467f92b21fb51857998298f
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-06-09 20:53:11 +0300

    print backwards

--- recursion/recursive_print.py
+++ recursion/recursive_print.py
@@ -7,3 +7,16 @@ def print_forward(array,index):
     print_forward(array,index+1)
 
 print_forward([1,2,3,4,5,6],0)
+
+
+
+def print_backward(array,index):
+    if len(array)==index:
+        return 
+    
+    print_backward(array,index+1)
+    print(array[index])
+
+print("BACKWARDS \n")
+
+print_backward([1,2,3,4,5,6],0)
